5f33fbce94e22197408bdbfa9a48f154c07872f2
danigb <danigb@gmail.com>
2011-08-25 05:36:32 +0200

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leaker::Application.config.secret_token = 'ab85b7861a3912ff312699fd9f7b242185db80eef777d86bf9b253a64b0e0c217262fac69c485cbb878182cd577b6300abcfed389068630c98bc08fb3f9a5cf3'
